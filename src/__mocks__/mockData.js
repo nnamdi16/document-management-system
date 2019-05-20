6691e3fc4ac6c6e35c59e8024ecf2420b1b1d40e
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-05-20 09:08:38 +0100

    Modify mockData response to mirror the response in the database

--- src/__mocks__/mockData.js
+++ src/__mocks__/mockData.js
@@ -1,10 +1,10 @@
 const mockData = {
 	authResponse: {
+		token:
+			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJleHAiOjE1MTM5MzkzMDIsImN1cnJlbnRVc2VyIjp7InVzZXJJZCI6MSwidXNlcm5hbWUiOiJydWtraWV5In0sImlhdCI6MTUxMzg1MjkwMn0.K2P7BAKDkMbk9avQznEE4u8PRtrx3P0mlSzLvFAdH2E',
 		status: true,
-		message: 'You have successfully signed up',
-		data: {
-			token:
-				'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJleHAiOjE1MTM5MzkzMDIsImN1cnJlbnRVc2VyIjp7InVzZXJJZCI6MSwidXNlcm5hbWUiOiJydWtraWV5In0sImlhdCI6MTUxMzg1MjkwMn0.K2P7BAKDkMbk9avQznEE4u8PRtrx3P0mlSzLvFAdH2E'
+		result: {
+			message: 'You have successfully signed up'
 		}
 	},
 	signupData: {
