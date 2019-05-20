77d8954745297416b59bd56d7382e252e58be550
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-05-20 03:40:06 +0100

    Test all reducer to check if they return the required output at different action types

---
+++ src/reducers/__test__/reducer.test.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-undef */
+import reducer from 'reducers/auth';
+import * as types from 'actions/types';
+import mockData from '__mocks__/mockData';
+
+describe('should return the initial state', () => {
+	it('should when state is undefined', () => {
+		expect(reducer(undefined, {})).toEqual({
+			authenticated: '',
+			errorMessage: ''
+		});
+	});
+
+	it('should handle AUTH_USER', () => {
+		expect(
+			reducer(
+				{},
+				{
+					type: types.AUTH_USER,
+					payload: mockData.authResponse.data.token
+				}
+			)
+		).toEqual({
+			authenticated: mockData.authResponse.data.token
+		});
+	});
+
+	it('should handle AUTH_ERROR', () => {
+		expect(
+			reducer(
+				{},
+				{
+					type: types.AUTH_ERROR,
+					payload: 'e.message'
+				}
+			)
+		).toEqual({
+			errorMessage: 'e.message'
+		});
+	});
+});
