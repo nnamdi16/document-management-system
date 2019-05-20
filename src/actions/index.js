a7eca202603e2e48db583d36ec1cd9f236e61501
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-05-20 09:11:27 +0100

    Modify the payload to suit the response gotten from the database

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,8 +6,8 @@ export const signUp = (formProps, callback) => async dispatch => {
 	try {
 		const response = await axios.post('http://206.189.44.170/api/users', formProps);
 		console.log(response);
-		dispatch({ type: AUTH_USER, payload: response.data.data.token });
-		localStorage.setItem('token', response.data.data.token);
+		dispatch({ type: AUTH_USER, payload: response.data.token });
+		localStorage.setItem('token', response.data.token);
 		console.log(localStorage);
 		callback();
 	} catch (e) {
