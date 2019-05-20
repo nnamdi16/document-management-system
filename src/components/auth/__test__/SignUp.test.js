355d6d5aa1e6fe49a69514dc5a1adef9fed1c88e
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-05-20 03:36:17 +0100

    Test SignUp component as well as simmulating the submit event

---
+++ src/components/auth/__test__/SignUp.test.js
@@ -0,0 +1,59 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import '../../../../setupTests';
+import { mount } from 'enzyme';
+import moxios from 'moxios';
+import mockAxios from 'axios';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import SignUp from 'components/auth/SignUp';
+import Root from '../../../Root';
+import { signUp } from 'actions/index';
+import mockData from '__mocks__/mockData';
+
+it('calls onSubmit prop function when form is submitted', () => {
+	const onSubmit = jest.fn();
+
+	const wrapper = mount(
+    <Root>
+        <SignUp handleSubmit={ onSubmit } />
+    </Root>
+	);
+
+	const form = wrapper.find('form');
+	form.simulate('submit');
+
+	expect(onSubmit).toHaveBeenCalledTimes(1);
+});
+
+describe('Simulate the submit event', () => {
+	beforeEach(() => {
+		moxios.install();
+		moxios.stubRequest('http://206.189.44.170/api/users', {
+			status: 200,
+			response: mockData.authResponse
+		});
+	});
+
+	afterEach(() => {
+		moxios.uninstall();
+	});
+
+	it('can post user data to the database and sign up a user', done => {
+		const wrapped = mount(
+    <Root>
+        <SignUp />
+    </Root>
+		);
+		//console.log(wrapped);
+		const container = wrapped
+			.find('.signUp') //.at(1);
+			.simulate('submit');
+		//console.log(container.html());
+		moxios.wait(() => {
+			wrapped.update();
+			//console.log(wrapped.find('.container').html());
+			expect(wrapped.find('.errorMessage').text()).toBe(2);
+		});
+	});
+});
