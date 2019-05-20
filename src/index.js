63f98b6bacc46de5c11d5522d0a3865d405b4588
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-05-20 03:18:56 +0100

    Add BrowserRouter and other components for creating routes to different components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'index.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Root from './Root';
+// import * as serviceWorker from './serviceWorker';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize';
+import './index.css';
 import App from 'components/App';
-import * as serviceWorker from 'serviceWorker';
+import LandingPage from 'components/landingPage/LandingPage';
+import DashBoard from 'components/DashBoard';
+import SignUp from 'components/auth/SignUp';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Root>
+        <App>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={ LandingPage } />
+                    <Route path="/signup" exact component={ SignUp } />
+                    <Route path="/dashboard" exact component={ DashBoard } />
+                </div>
+            </BrowserRouter>
+        </App>
+    </Root>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.register();
